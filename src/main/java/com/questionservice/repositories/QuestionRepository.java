f60f31b6646097364a8b1d5ee4ad97392782ef1e
pankajpc15 <pankaj21003@gmail.com>
2024-06-12 20:01:59 +0530

    questionservice commmit

---
+++ src/main/java/com/questionservice/repositories/QuestionRepository.java
@@ -0,0 +1,12 @@
+package com.questionservice.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.questionservice.entities.Question;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+		List<Question> findByQuizId(Long quizId);
+}
